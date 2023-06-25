714b8e1bc406217431b3eaecc2acd6daeeea77d3
roq-ai-snapshot <fabian.wesner@roq.tech>
2023-06-25 09:56:48 +0000

    Initial commit

---
+++ src/interfaces/restaurant/index.ts
@@ -0,0 +1,34 @@
+import { MenuItemInterface } from 'interfaces/menu-item';
+import { OrderInterface } from 'interfaces/order';
+import { TableInterface } from 'interfaces/table';
+import { UserInterface } from 'interfaces/user';
+import { GetQueryInterface } from 'interfaces';
+
+export interface RestaurantInterface {
+  id?: string;
+  description?: string;
+  image?: string;
+  name: string;
+  created_at?: any;
+  updated_at?: any;
+  user_id: string;
+  tenant_id: string;
+  menu_item?: MenuItemInterface[];
+  order?: OrderInterface[];
+  table?: TableInterface[];
+  user?: UserInterface;
+  _count?: {
+    menu_item?: number;
+    order?: number;
+    table?: number;
+  };
+}
+
+export interface RestaurantGetQueryInterface extends GetQueryInterface {
+  id?: string;
+  description?: string;
+  image?: string;
+  name?: string;
+  user_id?: string;
+  tenant_id?: string;
+}
